3cd6ceeea3860b6df703ecc89d4da64f7e611fdf
Azrog <78275913+azrogdev@users.noreply.github.com>
2024-03-05 17:45:44 +0100

    Update Player.js

--- package/ttt/dist/classes/Player.js
+++ package/ttt/dist/classes/Player.js
@@ -7,13 +7,15 @@ class Player {
         this.bot = (_a = options.bot) !== null && _a !== void 0 ? _a : false;
         this.symbol = (_b = options.symbol) !== null && _b !== void 0 ? _b : undefined;
     }
-    move(x, y, instance) {
-        if (!instance.data.started || instance.data.ended)
+    move(x, y) {
+        const instance = this.instance;
+        if (!instance.data.started || instance.data.ended || instance.data.currentPlayer !== this.id || this.bot)
             return;
         if (x > 2 || x < 0 || y > 2 || y < 0)
             throw new Error('Invalid move');
         if (Number(instance.grid[y][x]) !== 0)
-            return instance.emit(Events.MoveMade, { sucess: false });
+            return instance.emit(Events.Move, { sucess: false });
+        return instance.emit(Events.Move, { sucess: true, x: x, y: y, symbol: this.symbol });
     }
 }
 export { Player };
