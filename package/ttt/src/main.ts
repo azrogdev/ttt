55bbeca03b285d1edc580aaa1412d22bac61001c
Azrog <78275913+azrogdev@users.noreply.github.com>
2024-03-05 17:40:50 +0100

    Update main.ts

--- package/ttt/src/main.ts
+++ package/ttt/src/main.ts
@@ -1,3 +1,3 @@
 export { EventEmitter, Events } from './classes/EventEmitter.js';
 export { Player, PlayerOptions } from './classes/Player.js';
-export { TicTacToe, GameData, GameInstance, GameOptions, GetterPlayer } from './classes/TicTacToe.js';
+export { TicTacToe, GameData, GameInstance, GameOptions, GetterPlayer, MoveData, MoveScore } from './classes/TicTacToe.js';
