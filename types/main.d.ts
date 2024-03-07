d8e2d7f0f2d66bc47c54886edd2be5f954969e83
Azrog <78275913+azrogdev@users.noreply.github.com>
2024-03-07 16:11:17 +0100

    Delete types directory

--- types/main.d.ts
+++
@@ -1,4 +0,0 @@
-export { EventEmitter, Events } from './base/EventEmitter.js';
-export { Player, PlayerOptions } from './base/Player.js';
-export { TicTacToe, GameData, GameInstance, GameOptions, GetterPlayer, MoveData, MoveScore } from './base/TicTacToe.js';
-export { Save, Restore, validateProperty } from './base/Functions.js';
